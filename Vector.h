347ec25c85e8bdee3386d6d23b7642db8a3e503f
zhusong1020 <1225008748@qq.com>
2022-03-13 20:05:38 +0800

    finish vector

--- Vector.h
+++ Vector.h
@@ -4,15 +4,38 @@
 template <typename Object>
 class Vector
 {
+public:
 	explicit Vector(int initSize = 0);
 	Vector(const Vector& rhs) ;
+	Vector(Vector&& rhs) noexcept;
 	Vector& operator= (const Vector& rhs);
+	Vector& operator= (Vector&& rhs) noexcept;
+	void resize(int newSize);
+	void reserve(int newCapacity);
+	Object& operator [](int index);
+	const Object& operator [](int index) const;
+	bool empty() const;
+	int size() const;
+	int capacity() const;
+	void push_back(const Object &x);
+	void pop_back();
+	const Object& back() const;
 	~Vector();
+
+
+	Object* begin();
+
+	const Object* begin() const;
+
+	Object* end();
+
+	const Object* end() const;
 
 	static const int SPARE_CAPCITY = 16;
 private:
 	int theSize;
-	int theCapcity;
+	int theCapacity;
 	Object* objects;
 };
 
+template class Vector<int>;
--- main.cpp
+++ main.cpp
@@ -1,7 +1,11 @@
 #include <iostream>
 #include "Vector.h"
+#include <vector>
+using namespace std;
+
 
 int main()
 {
-
+	Vector<int> *v = new Vector<int>(100);
+	cin.get();
 }
